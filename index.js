413bf63712491f675314118681a6b3bd7a7a5d7d
Gordon Caister <gordoncaister@gmail.com>
2020-05-07 07:41:12 -0500

    set up server for hosting on heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config();
 const server = require("./server")
+const port = process.env.PORT || 5000;
 
-server.listen(5000, () => {
-    console.log("\n*** Server Running on http://localhost:5000 ***\n");
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
   })
